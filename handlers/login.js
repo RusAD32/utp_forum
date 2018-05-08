d2cc6c3b62854409bf97339e6644c6d56c8261cc
Vseslav Kochenov <kvv@tcinet.ru>
2018-05-08 23:06:32 +0300

    Permanent cookies and "Remember me"

--- handlers/login.js
+++ handlers/login.js
@@ -7,8 +7,9 @@ const handler = (reqest, responce, data, state) => {
     let users = JSON.parse(fs.readFileSync("./data/users.json", "UTF-8"));
     let username = "";
     let password = "";
+    let json_data = {};
     try {
-        let json_data = JSON.parse(data);
+        json_data = JSON.parse(data);
         username = json_data["username"];
         password = json_data["password"];
     } catch (e) {
@@ -21,8 +22,14 @@ const handler = (reqest, responce, data, state) => {
     let hash = crypto.createHash("sha256", "kharnin").update(password).digest("hex");
     if (users[username] === hash) {
         let cookie = username + new Date().getTime();
+        let expires = "";
+        if (json_data["remember"]) {
+            let d = new Date();
+            d.setMonth(d.getMonth()+12);
+            expires = "; Expires=" +d.toUTCString();
+        }
         state.cookies[cookie] = username;
-        responce.writeHead(200, {"content-type": "text/plain", "Set-cookie": "forum_session=" + cookie});
+        responce.writeHead(200, {"content-type": "text/plain", "Set-cookie": "forum_session=" + cookie + expires});
         responce.write("/forum");
         responce.end();
     } else {
